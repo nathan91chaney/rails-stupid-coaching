691f30b1cd1a35782067860a0877e18973832fff
nathan chaney <nathan91chaney@gmail.com>
2022-05-23 15:36:29 +0800

    some shit css

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    if params[:question].downcase == ""
+      @answer = ""
+    elsif params[:question].end_with?("?")
+      @answer =  "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
